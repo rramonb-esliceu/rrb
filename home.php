1f423aa476a7e921f830a576c5c7b997c2e07751
rramonb-esliceu <rramonb@esliceu.net>
2025-03-02 20:26:01 +0100

    home con posts

--- home.php
+++ home.php
@@ -27,50 +27,55 @@
             <div class="row">
                 <section>
                     <div class="row">
+                        
+        <?php
+        if (have_posts()) {
+            global $wp_query;
+
+            the_posts_pagination(array(
+                'mid_size' => 2,
+                'prev_text' => __( 'Anterior', 'textdomain'),
+                'next_text' => __( 'Siguiente', 'textdomain'),
+            ));
+
+            while (have_posts()) {
+                the_post();
+        ?>
+
+                <div class="col-lg-6 mb-5">
+                    <picture class="thumbnail">
                         <?php
-                        if (have_posts()) {
-                            global $wp_query;
-
-                            the_posts_pagination(array(
-                                'mid_size' => 2,
-                                'prev_text' => __( 'Anterior', 'textdomain'),
-                                'next_text' => __( 'Siguiente', 'textdomain'),
-                            ));
-
-                            while (have_posts()) {
-                                the_post();
-                        ?>
-                        <div class="col-lg-6 mb-5">
-                            <picture class="thumbnail">
-                                <?php
-                                if (has_post_thumbnail()) {
-                                    the_post_thumbnail('medium', array('class' => 'rounded w-100'));
-                                } else {
-                                    _e('Sense imatge en el post');
-                                }
-                                ?>
-                            </picture>
-
-                            <a class="new-title" href="<?php the_permalink(); ?>">
-                                <p><?php the_title(); ?></p>
-                            </a>
-
-                            <p class="subtitle mb-sm-2">
-                                <?php the_excerpt(); ?>
-                            </p>
-                        </div>
-                        <?php
-                            }
-
-                            the_posts_pagination(array(
-                                'mid_size' => 2,
-                                'prev_text' => __( 'Anterior', 'textdomain'),
-                                'next_text' => __( 'Siguiente', 'textdomain'),
-                            ));
+                        if (has_post_thumbnail()) {
+                            the_post_thumbnail('medium', array('class' => 'rounded w-100'));
                         } else {
-                            _e('No hi ha posts disponibles');
+                            echo '<p>Sense imatge en el post</p>';
                         }
                         ?>
+                    </picture>
+
+                    <a class="new-title" href="<?php the_permalink(); ?>">
+                        <p><?php echo the_title(); ?></p>
+                    </a>
+
+                    <p class="subtitle mb-sm-2">
+                        <?php the_excerpt(); ?>
+                    </p>
+                </div>
+
+        <?php
+            }
+
+        the_posts_pagination(array(
+            'mid_size' => 2,
+            'prev_text' => __( 'Anterior', 'textdomain'),
+            'next_text' => __( 'Siguiente', 'textdomain'),
+        ));
+
+
+        } else {
+            echo '<p>No hi ha posts disponibles</p>';
+        }
+        ?>
                     </div>
                 </section>
             </div>
